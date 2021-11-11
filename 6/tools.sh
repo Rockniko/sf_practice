423a95af24d9b01f2126ab5fc91daa3852655d79
Rockniko <81306735+Rockniko@users.noreply.github.com>
2021-11-11 18:51:32 +0300

    Add files via upload

---
+++ 6/tools.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.9
+sudo apt-get install python3-pip
+sudo apt install python3-django
+sudo apt-get install python-dev postgresql-server-dev-all
+pip install psycopg2
